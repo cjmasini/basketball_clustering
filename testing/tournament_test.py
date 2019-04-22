850d1c32cb1682b92ef0fd70f9f833a599f4ca82
Riley Annis <rileyannis@gmail.com>
2019-04-22 12:01:58 -0400

    generalized predictors and fixed whoopsies

--- testing/tournament_test.py
+++ testing/tournament_test.py
@@ -12,82 +12,61 @@ def higher_seed_wins(t1, t2, year=2017):
 def lower_id_wins(t1, t2, year=2017):
     return t1 if t1.id < t2.id else t2
 
+
+def clustering_predictor(t1, t2, year=2017, func=None):
+    if func is not None:
+        cluster1 = func(t1.id, year)
+        cluster2 = func(t2.id, year)
+        wins1 = []
+        for team in cluster1:
+            wins1.append(get_wins(team[0], team[1]))
+        wins2 = []
+        for team in cluster2:
+            wins2.append(get_wins(team[0], team[1]))
+        try:
+            return t1 if 1.*sum(wins1)/len(wins1) >= 1.*sum(wins2)/len(wins2) else t2
+        except:
+            return t1
+    else:
+        print("ERROR: func not defined in predictor")
+
 def k_means_predictor(t1, t2, year=2017):
-    cluster1 = kmeans(t1.id, year)
-    cluster2 = kmeans(t2.id, year)
-    wins1 = []
-    for team in cluster1:
-        wins1.append(get_wins(team[0], team[1]))
-    wins2 = []
-    for team in cluster1:
-        wins2.append(get_wins(team[0], team[1]))
-    return t1 if 1.*sum(wins1)/len(wins1) >= 1.*sum(wins2)/len(wins2) else t2
+    return clustering_predictor(t1, t2, year, kmeans)
 
 def spectral_predictor(t1, t2, year=2017):
-    cluster1 = spectral(t1.id, year)
-    cluster2 = spectral(t2.id, year)
-    wins1 = []
-    for team in cluster1:
-        wins1.append(get_wins(team[0], team[1]))
-    wins2 = []
-    for team in cluster1:
-        wins2.append(get_wins(team[0], team[1]))
-    return t1 if 1.*sum(wins1)/len(wins1) >= 1.*sum(wins2)/len(wins2) else t2
+    return clustering_predictor(t1, t2, year, spectral)
 
 def hierarchical_predictor(t1, t2, year=2017):
-    cluster1 = hierarchical(t1.id, year)
-    cluster2 = hierarchical(t2.id, year)
-    wins1 = []
-    for team in cluster1:
-        wins1.append(get_wins(team[0], team[1]))
-    wins2 = []
-    for team in cluster1:
-        wins2.append(get_wins(team[0], team[1]))
-    return t1 if 1.*sum(wins1)/len(wins1) >= 1.*sum(wins2)/len(wins2) else t2
+    return clustering_predictor(t1, t2, year, hierarchical)
 
 def birch_predictor(t1, t2, year=2017):
-    cluster1 = birch(t1.id, year)
-    cluster2 = birch(t2.id, year)
-    wins1 = []
-    for team in cluster1:
-        wins1.append(get_wins(team[0], team[1]))
-    wins2 = []
-    for team in cluster1:
-        wins2.append(get_wins(team[0], team[1]))
-    return t1 if 1.*sum(wins1)/len(wins1) >= 1.*sum(wins2)/len(wins2) else t2
+    return clustering_predictor(t1, t2, year, birch)
 
 def pca_kmeans_predictor(t1, t2, year=2017):
-    cluster1 = pca_kmeans(t1.id, year)
-    cluster2 = pca_kmeans(t2.id, year)
-    wins1 = []
-    for team in cluster1:
-        wins1.append(get_wins(team[0], team[1]))
-    wins2 = []
-    for team in cluster1:
-        wins2.append(get_wins(team[0], team[1]))
-    return t1 if 1.*sum(wins1)/len(wins1) >= 1.*sum(wins2)/len(wins2) else t2
+    return clustering_predictor(t1, t2, year, pca_kmeans)
 
 def kneighbors_predictor(t1, t2, year=2017):
-    cluster1 = kneighbors(t1.id, year)
-    cluster2 = kneighbors(t2.id, year)
-    wins1 = []
-    for team in cluster1:
-        wins1.append(get_wins(team[0], team[1]))
-    wins2 = []
-    for team in cluster1:
-        wins2.append(get_wins(team[0], team[1]))
-    return t1 if 1.*sum(wins1)/len(wins1) >= 1.*sum(wins2)/len(wins2) else t2
+    return clustering_predictor(t1, t2, year, kneighbors)
 
-prediction_functions = {"Higher seed wins": higher_seed_wins, "Lower id wins": lower_id_wins, 'k-means': k_means_predictor,
-                        "spectral": spectral_predictor, "hierarchical", hierarchical_predictor, "birch", birch_predictor,
-                        "pca_k-means": pca_kmeans_predictor, "kneighbors": kneighbors_predictor}
+def pca_kneighbors_predictor(t1, t2, year=2017):
+    return clustering_predictor(t1, t2, year, pca_kneighbors)
+
+def pca_hierarchical_predictor(t1, t2, year=2017):
+    return clustering_predictor(t1, t2, year, pca_hierarchical)
+
+def pca_spectral_predictor(t1, t2, year=2017):
+    return clustering_predictor(t1, t2, year, pca_spectral)
+
+#"kneighbors": kneighbors_predictor, "Higher seed wins": higher_seed_wins, "Lower id wins": lower_id_wins, 'k-means': k_means_predictor,
+#"spectral": spectral_predictor, "hierarchical", hierarchical_predictor, "birch", birch_predictor, 
+prediction_functions = {"pca_k-means": pca_kmeans_predictor, "pca_kneighbors": pca_kneighbors_predictor,
+                        "pca_hierarchical": pca_hierarchical_predictor, "pca_spectral": pca_spectral_predictor}
 
 for name, prediction_function in prediction_functions.items():
     s = []
-    predict_year = 2017
-    #for predict_year in range(2003,2018):
-    for year in [i for i in range(2003,2018) if i != predict_year]:
-        filename = "{}dataMatrix.csv".format(year)
-        b = Bracket(filename,year)
-        s.append(b.score_tournament(prediction_function,year))
+    year = 2017
+    #for year in range(2003,2018):
+    filename = "{}dataMatrix.csv".format(year)
+    b = Bracket(filename,year)
+    s.append(b.score_tournament(prediction_function,year))
     print(name + " - Average Score: {:0.2f}".format(sum(s)/len(s)))
