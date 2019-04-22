9785c4753e66311986116cfbf70ba94d697e4565
Riley Annis <rileyannis@gmail.com>
2019-04-22 08:47:53 -0400

    pca kneighbors

--- testing/clustering.py
+++ testing/clustering.py
@@ -86,36 +86,44 @@ def pca_kmeans(team_id, year = 2017, n_clusters = 91):
 def pca_spectral(team_id, year = 2017, n_clusters = 160):
     return clustering(team_id, year = year, n_clusters = n_clusters, func=SpectralClustering, fittable=False, pca_flag=True)
 
-def pca_hierarchical(team_id, year = 2017, n_clusters = 160, pca_flag=False, linkage="ward"):
+def pca_hierarchical(team_id, year = 2017, n_clusters = 160, pca_flag=True, linkage="ward"):
     return clustering(team_id, year = year, n_clusters = n_clusters, func=AgglomerativeClustering, fittable=False, pca_flag=True, linkage=linkage)
 
 def pca_hierarchical_ward(team_id, year = 2017, n_clusters = 160):
-    return hierarchical(team_id, year, n_clusters, pca_flag=True, linkage="ward")
+    return pca_hierarchical(team_id, year, n_clusters, linkage="ward")
 
 def pca_hierarchical_complete(team_id, year = 2017, n_clusters = 160):
-    return hierarchical(team_id, year, n_clusters, pca_flag=True, linkage="complete")
+    return pca_hierarchical(team_id, year, n_clusters, linkage="complete")
 
 def pca_hierarchical_average(team_id, year = 2017, n_clusters = 91):
-    return hierarchical(team_id, year, n_clusters, pca_flag=True, linkage="average")
+    return pca_hierarchical(team_id, year, n_clusters, linkage="average")
 
 def pca_hierarchical_single(team_id, year = 2017, n_clusters = 91):
-    return hierarchical(team_id, year, n_clusters, pca_flag=True, linkage="single")
+    return pca_hierarchical(team_id, year, n_clusters, linkage="single")
 
 def pca_birch(team_id, year = 2017, n_clusters = 160):
     return clustering(team_id, year = year, n_clusters = n_clusters, func=Birch, fittable=True, pca_flag=True)
 
-def kneighbors(team_id, year = 2017, n_neighbors=10):
+def kneighbors(team_id, year = 2017, n_neighbors=10, pca_flag=False, pca_components=43):
     data = ld.load_normalized_data_without_year(year)
     data["team_id_copy"] = data.index
     data_without_year = data.drop("year", axis=1)
     data_without_year = data_without_year.drop("team_id_copy", axis=1)
     latest = ld.load_normalized_year_data(year)
     team = ld.load_normalized_team_data(team_id, year)
-    cluster = NearestNeighbors(n_neighbors=n_neighbors).fit(data_without_year.values).kneighbors([team.values[:-1]])[1][0]
+    if pca_flag:
+        fit_data, fit_team = pca(team_id, year = year, n_components=pca_components)
+    else:
+        fit_data = data_without_year.values
+        fit_team = team.values[:-1]
+    cluster = NearestNeighbors(n_neighbors=n_neighbors).fit(fit_data).kneighbors([fit_team])[1][0]
     teams = []
     for index in cluster:
         teams.append((int(data.iloc[index].team_id_copy),data.iloc[index].year))
     return teams
+
+def pca_kneighbors(team_id, year = 2017, n_neighbors=10, n_components=43):
+    return kneighbors(team_id, year, n_neighbors, True, n_components)
 
 #print(kmeans(1291)) #1243, 1374, 1291,1344 are other ids
 #print(spectral(1291))
@@ -124,6 +132,6 @@ def kneighbors(team_id, year = 2017, n_neighbors=10):
 #print(hierarchical_average(1291))
 #print(hierarchical_single(1291))
 #print(birch(1291))
-print(pca_kmeans(1291))
-print(kneighbors(1291))
+#print(pca_kmeans(1291))
+#print(kneighbors(1291))
 
